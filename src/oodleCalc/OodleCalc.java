f8da1bbc49b72d849550cdc8b2d1d57cc3ddd45f
Asher Wood <awood5659@gmail.com>
2014-12-10 17:01:26 -0800

    Fix oodle bug

--- src/oodleCalc/OodleCalc.java
+++ src/oodleCalc/OodleCalc.java
@@ -16,23 +16,29 @@ public class OodleCalc {
 		System.out.print("How many things in an oodle?: ");
 		oodle = keyboard.nextInt();
 		int max_cars = (int) Math.floor((Cy*oodle)/3);
+		double max_oodle_cars = (Math.floor((Cy*oodle)/3))/oodle;
 		System.out.println("There are " + oodle + " of things in an oodle.");
+		System.out.println(max_oodle_cars + " oodles of cars were made.");
+		System.out.println(max_cars + " cars were made.");
 		System.out.println("Bodies left:\t" + (B*oodle - (max_cars)*1));
 		System.out.println("Cylinders left:\t" + (Cy*oodle - (max_cars)*3));
 		System.out.println("Engines left:\t" + (E*oodle - (max_cars)*1));
 		System.out.println("Tires left:\t" + (Tr*oodle - (max_cars)*4));
-		System.out.println(max_cars + " cars were made.");
+		
 		
 		
 		while (oodle != (double) 1) {
 			System.out.print("How many things in an oodle?: ");
 			oodle = keyboard.nextInt();
+			max_cars = (int) Math.floor((Cy*oodle)/3);
+			max_oodle_cars = (Math.floor((Cy*oodle)/3))/oodle;
 			System.out.println("There are " + oodle + " of things in an oodle.");
-			System.out.println("Bodies left:\t" + (B*oodle % 1));
-			System.out.println("Cylinders left:\t" + (Cy*oodle % 3));
-			System.out.println("Engines left:\t" + (E*oodle % 1));
-			System.out.println("Tires left:\t" + (Tr*oodle % 4));
-			System.out.println(((int)Math.floor((Cy*oodle)/3)) + " oodles of cars were made.");
+			System.out.println(max_oodle_cars + " oodles of cars were made.");
+			System.out.println(max_cars + " cars were made.");
+			System.out.println("Bodies left:\t" + (B*oodle - (max_cars)*1));
+			System.out.println("Cylinders left:\t" + (Cy*oodle - (max_cars)*3));
+			System.out.println("Engines left:\t" + (E*oodle - (max_cars)*1));
+			System.out.println("Tires left:\t" + (Tr*oodle - (max_cars)*4));
 		}
 	}
 }
